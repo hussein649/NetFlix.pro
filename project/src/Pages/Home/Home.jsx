5f6a0036b5f9da310290a554d715b574cff49063
hussein649 <husseinhebo@gmail.com>
2024-01-30 22:17:40 +0100

    added Banner

--- project/src/Pages/Home/Home.jsx
+++ project/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from '../../Components/Nav/Nav'
 import Footer from '../../Components/Footer/Footer'
+import Banner from '../../Components/Banner/Banner'
 const Home = () => {
   return (
       <div>
       <Nav />
+      <Banner />
       <Footer />
       
     </div>
